4e78da4170a3ccbb04d3fa738b62163a4b6cb4b7
Thomas Lepé <thomaslepe8@gmail.com>
2022-11-03 10:56:13 -0300

    thomas

---
+++ Contraseña.py
@@ -0,0 +1,23 @@
+import hashlib
+
+encontrada = 0
+input_hash = input("Inserte la contraseña hasheada: ")
+pass_doc = input("Inserte el diccionario: ")
+
+try:
+    pass_file = open(pass_doc, 'r')
+except:
+    print("Error:" + pass_doc + "No ha sido encontrada")
+
+for palabra in pass_file:
+    palabra_cifrada = palabra.encode('utf-8')
+    palabra_hasheada = hashlib.md5(palabra_cifrada.strip())
+    digest = palabra_hasheada.hexdigest()
+
+    if digest == input_hash:
+        print("Contraseña Econtrada \n La contraseña es: " + palabra)
+        econtrada = 1
+        break
+
+if not encontrada:
+    print("Contraseña no encontrada en el archivo " + pass_doc)
